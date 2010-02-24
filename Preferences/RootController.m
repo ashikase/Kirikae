b8d28941cf1d17d53daef0038fb9941f7477a7bb
gaizin <gaizin@ec0de7a4-9a3b-11de-b5b0-958946df26fc>
2010-02-24 01:57:28 +0000

    Prefs: Removed unused control-related code.

--- Preferences/RootController.m
+++ Preferences/RootController.m
@@ -3,7 +3,7 @@
  * Type: iPhone OS SpringBoard extension (MobileSubstrate-based)
  * Description: a task manager/switcher for iPhoneOS
  * Author: Lance Fetters (aka. ashikase)
- * Last-modified: 2010-02-21 15:11:04
+ * Last-modified: 2010-02-21 15:18:48
  */
 
 /**
@@ -52,7 +52,6 @@
 
 #import "AppearanceController.h"
 #import "Constants.h"
-#import "ControlController.h"
 #import "DocumentationController.h"
 #import "GeneralController.h"
 #import "HtmlDocController.h"
